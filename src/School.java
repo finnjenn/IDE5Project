ad58e720368ba0ff16b58d5b69e7fa9a426e1edd
finnjenn <finnjennn@gmail.com>
2021-12-09 13:20:34 -0500

    added interface file Callable

--- src/School.java
+++ src/School.java
@@ -1,7 +1,7 @@
 abstract public class School {
     private int enrollment;
     public abstract void describeLevel();
-    pubic void setEnrollment(int students){
+    public void setEnrollment(int students){
         enrollment = students;
     }
     public int getEnrollment() {
