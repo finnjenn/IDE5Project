63c9877c1f743a140cdf9ec49af9fa457568014b
finnjenn <finnjennn@gmail.com>
2021-12-11 02:38:21 -0500

    added all the rest of the elementary schools. i need to find out their enrollment sizes tomorrow

--- src/Main.java
+++ src/Main.java
@@ -15,37 +15,56 @@ public class Main implements Callable{
         Elementaryschool pricesF = new Elementaryschool();
         Elementaryschool aubElm = new Elementaryschool();
         Elementaryschool eastMelm = new Elementaryschool();
+        Elementaryschool belv = new Elementaryschool();
+        Elementaryschool fallb = new Elementaryschool();
+        Elementaryschool gilb = new Elementaryschool();
+        Elementaryschool hardave = new Elementaryschool();
+        Elementaryschool kipps = new Elementaryschool();
+        Elementaryschool margb = new Elementaryschool();
 
-        //Setting names for each high school
+
+        //Setting names for each high school object
         cburgHigh.setName("Christiansburg High School");
         bburgHigh.setName("Blacksburg High School");
         aubHigh.setName("Auburn High School");
         eastHigh.setName("Eastern Montgomery High School");
-        //Setting enrollment for each high school
+        //Setting enrollment for each high school object
         cburgHigh.setEnrollment(1019);
         bburgHigh.setEnrollment(1199);
         aubHigh.setEnrollment(414);
         eastHigh.setEnrollment(289);
-        //Setting names for each middle school
+        //Setting names for each middle school obejct
         cburgMid.setName("Christiansburg Middle School");
         bburgMid.setName("Blacksburg Middle School");
         aubMid.setName("Auburn Middle School");
         shawMid.setName("Shawsville Middle School");
-        //Setting enrollment for each middle school
+        //Setting enrollment for each middle school object
         cburgMid.setEnrollment(832);
         bburgMid.setEnrollment(959);
         aubMid.setEnrollment(574);
         shawMid.setEnrollment(218);
-        //Setting names for each elementary school
+        //Setting names for each elementary school object
         cburgElm.setName("Christiansburg Elementary School");
         pricesF.setName("Prices Fork Elementary School");
         aubElm.setName("Auburn Elementary School");
         eastMelm.setName("Eastern Montgomery Elementary School");
-        //Setting enrollment for each elementary school
+        belv.setName("Belview Elementary School");
+        gilb.setName("Gilbert Linkous Elementary School");
+        margb.setName("Margaret Beeks Elementary School");
+        fallb.setName("Falling Branch Elementary School");
+        hardave.setName("Harding Avenue Elementary School");
+        kipps.setName("Kipps Elementary School");
+        //Setting enrollment for each elementary school object
         cburgElm.setEnrollment(388);
         pricesF.setEnrollment(459);
         aubElm.setEnrollment(552);
         eastMelm.setEnrollment(456);
+        belv.setEnrollment(456);
+        gilb.setEnrollment(456);
+        margb.setEnrollment(456);
+        fallb.setEnrollment(456);
+        hardave.setEnrollment(456);
+        kipps.setEnrollment(456);
         //Main Output
         System.out.println("School System: " + SYSTEMNAME); //outputs string constants from Callable interface
         System.out.println("Superintendent: " + SUPERI);
@@ -98,6 +117,30 @@ public class Main implements Callable{
         System.out.println(eastMelm.getName());
         System.out.println("Enrollment is " + eastMelm.getEnrollment());
         eastMelm.describeLevel();
+
+        System.out.println(belv.getName());
+        System.out.println("Enrollment is " + belv.getEnrollment());
+        belv.describeLevel();
+
+        System.out.println(gilb.getName());
+        System.out.println("Enrollment is " + gilb.getEnrollment());
+        gilb.describeLevel();
+
+        System.out.println(kipps.getName());
+        System.out.println("Enrollment is " + kipps.getEnrollment());
+        kipps.describeLevel();
+
+        System.out.println(fallb.getName());
+        System.out.println("Enrollment is " + fallb.getEnrollment());
+        fallb.describeLevel();
+
+        System.out.println(margb.getName());
+        System.out.println("Enrollment is " + margb.getEnrollment());
+        margb.describeLevel();
+
+        System.out.println(hardave.getName());
+        System.out.println("Enrollment is " + hardave.getEnrollment());
+        hardave.describeLevel();
 
 
     }
